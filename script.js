b8067746a8809777f0007f949fc33c73376bfd36
Unknown <EBoblett949@west-mec.org>
2018-08-16 15:25:37 -0700

    Started the project
    Stared the project.

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+    Project 01_06_01
+      Author: Eli Boblett
+      Date: 8.16.18
+      Filename: script.js
+*/ 
+
+"use strict"
+
+var formValidity = true;
+
+function validateForm() {
+
+}
+
+function createEventListeners() {
+    validateForm();
+}
+
